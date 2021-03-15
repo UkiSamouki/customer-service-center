085309b82ae23b990ab5c6f595edf4ab32acb814
Uros Zeravica <uros.zeravica@telenor.rs>
2021-03-16 00:49:56 +0100

    making folder structure with front and backend

---
+++ csc_backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c0bf454e9ba5114f584b1c4cf1e663b1'),
+    },
+  },
+});
---
+++ csc_backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c71bd3f6-70e9-47ca-89ea-2f713ce017ea'
+};
